91325cbde9bcd0f433a06c98ba55c72a3140d982
Bálint Bokros <balint.bokros@avatao.com>
2017-06-12 13:47:58 +0200

    Remove unnecessary DataBase class

--- lib/models/data.ts
+++ lib/models/data.ts
@@ -7,15 +7,3 @@ export interface DataJSON {
 export interface DataAPI {}
 
 export interface Data extends DataJSON, DataAPI {}
-
-export class DataBase implements Data {
-    public name: string;
-    public value: string | number | boolean;
-    public prompt: string;
-
-    constructor(data: DataJSON) {
-        this.name = data.name;
-        this.value = data.value;
-        this.prompt = data.prompt;
-    }
-}
