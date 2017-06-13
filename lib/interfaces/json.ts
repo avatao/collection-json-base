6c30aad7ff1d236357186508b9f1d200746d0438
Bálint Bokros <balint.bokros@avatao.com>
2017-06-13 10:00:51 +0200

    Check optional values in JSON interfaces properly

--- lib/interfaces/json.ts
+++ lib/interfaces/json.ts
@@ -7,45 +7,45 @@ import { Template } from './template';
 export interface CollectionJSON {
     version: string;
     href: string;
-    links: Link[];
-    items: Item[];
-    queries: Query[];
-    template: Template;
-    error: Error;
+    links?: Link[];
+    items?: Item[];
+    queries?: Query[];
+    template?: Template;
+    error?: Error;
 }
 
 export interface DataJSON {
     name: string;
-    value: string | number | boolean;
-    prompt: string;
+    value?: string | number | boolean;
+    prompt?: string;
 }
 
 export interface ErrorJSON {
-    title: string;
-    code: string;
-    message: string;
+    title?: string;
+    code?: string;
+    message?: string;
 }
 
 export interface ItemJSON {
     href: string;
-    links: Link[];
-    data: Data[];
+    links?: Link[];
+    data?: Data[];
 }
 
 export interface LinkJSON {
     href: string;
     rel: string;
-    name: string;
-    prompt: string;
-    render: string;
+    name?: string;
+    prompt?: string;
+    render?: string;
 }
 
 export interface QueryJSON {
     href: string;
     rel: string;
-    name: string;
-    prompt: string;
-    data: Data[];
+    name?: string;
+    prompt?: string;
+    data?: Data[];
 }
 
 export interface TemplateJSON {
